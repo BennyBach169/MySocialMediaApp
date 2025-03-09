996a3d1e856ad6f49c3be2afc8ec64e3859778e2
BennyBach169 <BennyBach777@gmail.com>
2025-03-09 12:14:59 +0200

    Update FeedService.ts

--- social_media_app/src/services/FeedService.ts
+++ social_media_app/src/services/FeedService.ts
@@ -6,41 +6,41 @@ import { User } from "../models/User";
 
 class FeedService{
     async getAllPosts(){
-        return (await axios.get<Post[]>(`http://192.168.1.105:8080/api/feed/posts`)).data
+        return (await axios.get<Post[]>(`http://localhost:8080/api/feed/posts`)).data
     }
     async getPostComments(postId:number){
-        return (await axios.get<PostComment[]>(`http://192.168.1.105:8080/api/feed/postcomments/${postId}`)).data
+        return (await axios.get<PostComment[]>(`http://localhost:8080/api/feed/postcomments/${postId}`)).data
 
     }
     async getPost(postId:number){
-        return (await axios.get<Post>(`http://192.168.1.105:8080/api/feed/getpost/${postId}`)).data
+        return (await axios.get<Post>(`http://localhost:8080/api/feed/getpost/${postId}`)).data
 
     }
 
     async getPostCommentsCount(postId:number){
-        return (await axios.get<number>(`http://192.168.1.105:8080/api/feed/getCommentsCount/${postId}`)).data
+        return (await axios.get<number>(`http://localhost:8080/api/feed/getCommentsCount/${postId}`)).data
 
     }
 
     async getUserById(userId:number){
-        return (await axios.get<User>(`http://192.168.1.105:8080/api/feed/getuserbyid/${userId}`)).data
+        return (await axios.get<User>(`http://localhost:8080/api/feed/getuserbyid/${userId}`)).data
 
     }
 
     async getUsersPostsById(userId:number){
-        return (await axios.get<Post[]>(`http://192.168.1.105:8080/api/feed/getusersposts/${userId}`)).data
+        return (await axios.get<Post[]>(`http://localhost:8080/api/feed/getusersposts/${userId}`)).data
     }
 
     async getAllFollwers(userId:number){
-        return (await axios.get<User[]>(`http://192.168.1.105:8080/api/feed/getuserfollowers/${userId}`)).data
+        return (await axios.get<User[]>(`http://localhost:8080/api/feed/getuserfollowers/${userId}`)).data
       }
     
       async getAllFollwings(userId:number){
-        return (await axios.get<User[]>(`http://192.168.1.105:8080/api/feed/getuserfollowings/${userId}`)).data
+        return (await axios.get<User[]>(`http://localhost:8080/api/feed/getuserfollowings/${userId}`)).data
       }
 
       async findAllPostLikers(postId:number){
-        return (await axios.get<User[]>(`http://192.168.1.105:8080/api/feed/findallpostlikers/${postId}`)).data
+        return (await axios.get<User[]>(`http://localhost:8080/api/feed/findallpostlikers/${postId}`)).data
       }
   
 }
