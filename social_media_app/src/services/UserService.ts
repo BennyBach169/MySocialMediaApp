a03f311d7a22cd10d1a8fdcf3675d7491545c396
BennyBach169 <BennyBach777@gmail.com>
2025-03-09 12:16:37 +0200

    Update UserService.ts

--- social_media_app/src/services/UserService.ts
+++ social_media_app/src/services/UserService.ts
@@ -11,58 +11,58 @@ class UserService {
 //       })
 //     ).data;
 //   }
-    return (await axios.get<User>(`http://192.168.1.105:8080/api/user/details`)).data
+    return (await axios.get<User>(`http://localhost:8080/api/user/details`)).data
   }
   
   async addPost(post:Post){
-    return (await axios.post<User>(`http://192.168.1.105:8080/api/user/addpost`,post)).data
+    return (await axios.post<User>(`http://localhost:8080/api/user/addpost`,post)).data
   }
 
   async checkIfPostLiked(postId:number){
-    return (await axios.get<boolean>(`http://192.168.1.105:8080/api/user/checkIfPostLiked/${postId}`)).data
+    return (await axios.get<boolean>(`http://localhost:8080/api/user/checkIfPostLiked/${postId}`)).data
   }
   async likeUnlike(post:Post){
-    return (await axios.post<boolean>(`http://192.168.1.105:8080/api/user/post/likeunlike`,post)).data
+    return (await axios.post<boolean>(`http://localhost:8080/api/user/post/likeunlike`,post)).data
   }
 
   async getUserPosts(){
-    return (await axios.get<Post[]>(`http://192.168.1.105:8080/api/user/posts`)).data
+    return (await axios.get<Post[]>(`http://localhost:8080/api/user/posts`)).data
   }
   async commentOnPost(postComment:PostComment){
-    return (await axios.post(`http://192.168.1.105:8080/api/user/post/comment`,postComment)).data
+    return (await axios.post(`http://localhost:8080/api/user/post/comment`,postComment)).data
   }
 
   async updateUser(user:User){
-    return (await axios.put(`http://192.168.1.105:8080/api/user/updateuser`,user)).data
+    return (await axios.put(`http://localhost:8080/api/user/updateuser`,user)).data
   }
 
   async followUnfollow(followedId:number){
-    return (await axios.post<boolean>(`http://192.168.1.105:8080/api/user/followunfollow/${followedId}`)).data
+    return (await axios.post<boolean>(`http://localhost:8080/api/user/followunfollow/${followedId}`)).data
   }
   async checkIfFollowing(followedId:number){
-    return (await axios.get<boolean>(`http://192.168.1.105:8080/api/user/checkIfFollowing/${followedId}`)).data
+    return (await axios.get<boolean>(`http://localhost:8080/api/user/checkIfFollowing/${followedId}`)).data
   }
   async editPost(post:Post){
-    return (await axios.put(`http://192.168.1.105:8080/api/user/editpost`,post)).data
+    return (await axios.put(`http://localhost:8080/api/user/editpost`,post)).data
   }
   async deletePost(postId:number){
-    return (await axios.delete(`http://192.168.1.105:8080/api/user/deletepost/${postId}`)).data
+    return (await axios.delete(`http://localhost:8080/api/user/deletepost/${postId}`)).data
   }
 
   async getAllFollwers(){
-    return (await axios.get<User[]>(`http://192.168.1.105:8080/api/user/followers`)).data
+    return (await axios.get<User[]>(`http://localhost:8080/api/user/followers`)).data
   }
 
   async getAllFollwings(){
-    return (await axios.get<User[]>(`http://192.168.1.105:8080/api/user/followings`)).data
+    return (await axios.get<User[]>(`http://localhost:8080/api/user/followings`)).data
   }
 
   async deletePostComment(commentId:number){
-    return (await axios.delete(`http://192.168.1.105:8080/api/user/post/deletecomment/${commentId}`)).data
+    return (await axios.delete(`http://localhost:8080/api/user/post/deletecomment/${commentId}`)).data
   }
 
   async getFollowingFeed(){
-    return (await axios.get<Post[]>(`http://192.168.1.105:8080/api/user/postsofollowings`)).data
+    return (await axios.get<Post[]>(`http://localhost:8080/api/user/postsofollowings`)).data
   }
 
   
