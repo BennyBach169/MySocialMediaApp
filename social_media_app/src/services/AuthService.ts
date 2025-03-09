6b8ea13c1cd439928021f3950a4567baea5641b3
BennyBach169 <BennyBach777@gmail.com>
2025-03-09 12:14:12 +0200

    Update AuthService.ts

--- social_media_app/src/services/AuthService.ts
+++ social_media_app/src/services/AuthService.ts
@@ -4,23 +4,23 @@ import { User } from "../models/User";
 class AuthService{
 
     async login(email:string,password:string){
-        return (await axios.get<string>(`http://192.168.1.105:8080/auth/login?email=${email}&password=${password}`)).data
+        return (await axios.get<string>(`http://localhost:8080/auth/login?email=${email}&password=${password}`)).data
     }
 
     async logout(){
-        return (await axios.post<string>(`http://192.168.1.105:8080/auth/logout`)).data
+        return (await axios.post<string>(`http://localhost:8080/auth/logout`)).data
     }
 
     async register(user:User){
-        return (await axios.post(`http://192.168.1.105:8080/auth/register`,user)).data 
+        return (await axios.post(`http://localhost:8080/auth/register`,user)).data 
     }
 
     async adminLogin(email:string,password:string){
-        return (await axios.get<string>(`http://192.168.1.105:8080/auth/admin/api/login?email=${email}&password=${password}`)).data
+        return (await axios.get<string>(`http://localhost:8080/auth/admin/api/login?email=${email}&password=${password}`)).data
     }
 
     async adminLogout(token:string){
-        return (await axios.post(`http://192.168.1.105:8080/auth/admin/api/logout`,[],{ headers: { Authorization: "Bearer " + token } })).data
+        return (await axios.post(`http://localhost:8080/auth/admin/api/logout`,[],{ headers: { Authorization: "Bearer " + token } })).data
     }
 
 }
